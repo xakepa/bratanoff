6b1888b8a548f2a246f997542efa81ca585b1713
unknown <plamen_ivanov@mail.com>
2021-09-08 18:30:26 +0300

    scrolling effect added

---
+++ styles/effect.js
@@ -0,0 +1,40 @@
+$("#administrative").on("click", function () {
+  $(".wrap, a").toggleClass("active");
+
+  return false;
+});
+
+$(document).ready(function () {
+  $(".view-btn").click(function () {
+    $(this).text(
+      $(this).text() == "скрий текста" ? "Покажи повече" : "скрий текста"
+    );
+  });
+});
+
+$(".tips").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".protect-your-rights").offset().top,
+    },
+    "slow"
+  );
+});
+
+$(".go-to-laws").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".types-laws").offset().top,
+    },
+    "slow"
+  );
+});
+
+$(".go-to-top").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".header").offset().top,
+    },
+    "slow"
+  );
+});
